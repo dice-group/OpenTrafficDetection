f82d4f9fc764e4368ca29b27514ce80f718e5913
Geraldo S Jr <gsjunior86@gmail.com>
2021-07-07 19:21:47 +0200

    Update setup_training_darknet.sh

--- setup_training_darknet.sh
+++ setup_training_darknet.sh
@@ -1,11 +1,3 @@
 wget https://hobbitdata.informatik.uni-leipzig.de/VEP/training_data/vep.zip
 unzip vep.zip
 rm vep.zip
-cd vep/
-wget https://hobbitdata.informatik.uni-leipzig.de/VEP/training_data/test.zip
-wget https://hobbitdata.informatik.uni-leipzig.de/VEP/training_data/train.zip
-unzip test.zip
-unzip train.zip
-rm test.zip
-rm train.zip
-
