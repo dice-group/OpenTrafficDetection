c0128213a593d15dfd12201726bf3de96104c65c
Geraldo <gsjunior86@gmail.com>
2022-01-16 20:08:08 +0100

    create output file for detections and remote server

--- render_detect.py
+++ render_detect.py
@@ -1,119 +1,128 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import shutil
 from PIL import Image, ImageEnhance
 import time
 from pathlib import Path
+from datetime import datetime
+import ipaddress
+import socket
 import cv2
 import torch
 import torch.backends.cudnn as cudnn
 
-
-obj_colors = {
-    0: (0,255,255), #person
-    1: (255,0,0), # bicycle
-    2: (0,255,0), # car
-    3: (6,140,201), #motorbike
-    4: (255,51,51), #bus
-    5: (0,0,255) #truck
-}
-
-
-def detectForVideo(file,model):
-	cap = cv2.VideoCapture(file)	
-	ret, img = cap.read()
-#	scale_percent = 35 # percent of original size
-#	width = int(img.shape[1] * scale_percent / 100)
-#	height = int(img.shape[0] * scale_percent / 100)
-#	dim = (width, height)
+obj_colors = {  # person
+                # bicycle
+                # car
+                # motorbike
+                # bus
+                # truck
+    0: (0, 0xFF, 0xFF),
+    1: (0xFF, 0, 0),
+    2: (0, 0xFF, 0),
+    3: (6, 140, 201),
+    4: (0xFF, 51, 51),
+    5: (0, 0, 0xFF),
+    }
 
 
-	while (True):
-	    ret, frame = cap.read()
-	    	
-	    #frame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
-	    #frame = iu.adjust_gamma(frame,4.0)	
-	    t1 = time.time()*1000
-	    pred = model(frame,augment=True)
-	    results = pred.pandas().xyxy[0]
-		
-	    
-	    
-	    #bounding_box = cv2.selectROI('Multi-Object Tracker', frame, True,False)
-	    
-	#    print(bounding_box)
-	    results = results[results['class'].isin(obj_colors)]
-	    print(results)
-	    for index, row in results.iterrows():   
-		
-		#xywhs = row[:, 0:4]
-		
-		#outputs = deepsort.update(xywhs.cpu(), confs.cpu(), clss, frame)
-	    
-                xmin,ymin,xmax,ymax = row['xmin'],row['ymin'],row['xmax'],row['ymax']
-		
-		
-                cv2.rectangle(frame,(int(xmin),int(ymin)),(int(xmax),int(ymax)),obj_colors[row['class']],1)
-                cv2.rectangle(frame,(int(xmin),int(ymin)-15),(int(xmax),int(ymin)),obj_colors[row['class']],-1)
-                cv2.putText(frame, row['name'] + ': ' + str(row['confidence']),(int(xmin),int(ymin)-5), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 0), 4, cv2.LINE_AA)
-                cv2.putText(frame, row['name']+ ': ' + str(row['confidence']),(int(xmin),int(ymin)-5), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 255), 1,cv2.LINE_AA)
-		
-	    
-	    fps = (1000 / ((time.time()*1000)-t1))
-	    cv2.putText(frame, "FPS: %.2f" % fps,
-	    (10,15), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 1, cv2.LINE_AA)
-		
-	    
-	    cv2.imshow('frame',frame)
-	#    out.write(frame)
-	    if cv2.waitKey(1) & 0xFF == ord('q'):
-		    cap.release()
-		    cv2.destroyAllWindows()
-		    break
+def detectForVideo(file, model,output,ip_address,port):
+    if(file == 'camera'):
+        cap = cv2.VideoCapture(0)
+    else:     
+        cap = cv2.VideoCapture(file)
+    (ret, img) = cap.read()
 
-def detectForCamera(model):
-	vid = cv2.VideoCapture(0)
-	ret, frame = vid.read()
-	width = int(frame.shape[1] * 30 / 100)
-	height = int(frame.shape[0] * 30 / 100)
-	while (True):
-            ret, frame = vid.read()
-            frame = cv2.resize(frame, (width,height), interpolation = cv2.INTER_AREA)
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)	
-	    #frame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
-	    #frame = iu.adjust_gamma(frame,4.0)	
-            t1 = time.time()*1000
-            pred = model(frame,augment=True)
-            results = pred.pandas().xyxy[0]
-		
-	    
-	    
-	    #bounding_box = cv2.selectROI('Multi-Object Tracker', frame, True,False)
-	    
-	#    print(bounding_box)
-            results = results[results['class'].isin(obj_colors)]
-            print(results)
-            for index, row in results.iterrows():   
-		
-		#xywhs = row[:, 0:4]
-		
-		#outputs = deepsort.update(xywhs.cpu(), confs.cpu(), clss, frame)
-	    
-                xmin,ymin,xmax,ymax = row['xmin'],row['ymin'],row['xmax'],row['ymax']
-		
-		
-                cv2.rectangle(frame,(int(xmin),int(ymin)),(int(xmax),int(ymax)),obj_colors[row['class']],1)
-                cv2.rectangle(frame,(int(xmin),int(ymin)-15),(int(xmax),int(ymin)),obj_colors[row['class']],-1)
-                cv2.putText(frame, row['name'] + ': ' + str(row['confidence']),(int(xmin),int(ymin)-5), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 0), 4, cv2.LINE_AA)
-                cv2.putText(frame, row['name']+ ': ' + str(row['confidence']),(int(xmin),int(ymin)-5), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 255), 1,cv2.LINE_AA)
-		
-	    
-            fps = (1000 / ((time.time()*1000)-t1))
-            cv2.putText(frame, "FPS: %.2f" % fps,
-            (10,15), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 1, cv2.LINE_AA)
-		
-	    
-            cv2.imshow('frame',frame)
-	#    out.write(frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                    cv2.destroyAllWindows()
-                    break
+# ....scale_percent = 35 # percent of original size
+# ....width = int(img.shape[1] * scale_percent / 100)
+# ....height = int(img.shape[0] * scale_percent / 100)
+# ....dim = (width, height)
+
+    isip = False
+    try:
+        ip = ipaddress.ip_address(ip_address)
+        isip = True
+        sock = socket.socket(socket.AF_INET, # Internet
+                        socket.SOCK_DGRAM) # UDP
+        print("IP address {} is valid. The object returned is {}".format(ip_address, ip))
+    except ValueError:
+        print("IP address {} is not valid".format(ip_address)) 
+
+
+    while True:
+        (ret, frame) = cap.read()
+
+        # frame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
+        # frame = iu.adjust_gamma(frame,4.0)....
+
+        t1 = time.time() * 1000
+        pred = model(frame, augment=True)
+        results = pred.pandas().xyxy[0]
+
+        # bounding_box = cv2.selectROI('Multi-Object Tracker', frame, True,False)
+
+
+        results = results[results['class'].isin(obj_colors)]
+        names = ",".join(results['name'].tolist())
+        now = datetime.now()    
+        current_time = now.strftime("%H:%M:%S")
+        
+        message = current_time + "|" + names
+        with open(output, 'a') as f:
+            f.write(message)
+            f.write('\n')
+        
+        if(isip == True):
+            sock.sendto(message.encode(), (ip_address, int(port)))
+        
+        for (index, row) in results.iterrows():
+
+            (xmin, ymin, xmax, ymax) = (row['xmin'], row['ymin'], row['xmax'
+                                        ], row['ymax'])
+    
+            cv2.rectangle(frame, (int(xmin), int(ymin)), (int(xmax),
+                          int(ymax)), obj_colors[row['class']], 1)
+            cv2.rectangle(frame, (int(xmin), int(ymin) - 15), (int(xmax),
+                          int(ymin)), obj_colors[row['class']], -1)
+            cv2.putText(
+                frame,
+                row['name'] + ': ' + str(row['confidence']),
+                (int(xmin), int(ymin) - 5),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.4,
+                (0, 0, 0),
+                4,
+                cv2.LINE_AA,
+                )
+            cv2.putText(
+                frame,
+                row['name'] + ': ' + str(row['confidence']),
+                (int(xmin), int(ymin) - 5),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.4,
+                (0xFF, 0xFF, 0xFF),
+                1,
+                cv2.LINE_AA,
+                )
+    
+        fps = 1000 / (time.time() * 1000 - t1)
+        cv2.putText(
+                frame,
+                'FPS: %.2f' % fps,
+                (10, 15),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.6,
+                (0, 0xFF, 0),
+                1,
+                cv2.LINE_AA,
+                )
+        
+        cv2.imshow('frame', frame)
+        
+            #    out.write(frame)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cap.release()
+            cv2.destroyAllWindows()
+            break
--- vep_yolov5.py
+++ vep_yolov5.py
@@ -7,44 +7,34 @@ import platform
 import shutil
 from PIL import Image, ImageEnhance
 from render_detect import detectForVideo
-from render_detect import detectForCamera
 import time
 from pathlib import Path
 import cv2
 import torch
 import torch.backends.cudnn as cudnn
 
-
-
-
-
 parser = argparse.ArgumentParser()
-parser.add_argument('--source', type=str, default='camera', help='source')
-
+parser.add_argument('--source', type=str, default='camera', help='source video (camera or videofile)')
+parser.add_argument('--output', type=str, default='output_detection.txt', required=False,help='file output')
+parser.add_argument('--remote_server', type=str, required=False,help='ip and port to send results')
 
 args = parser.parse_args()
 
-
 # Model
-#model = torch.hub.load('ultralytics/yolov5', 'yolov5_vep')
+# model = torch.hub.load('ultralytics/yolov5', 'yolov5_vep')
 model = torch.hub.load('yolov5_config/', 'custom', path='yolov5_config/weights/best.pt', source='local') 
 model.conf = 0.5
 file = args.source
-if(file == 'camera'):
-	detectForCamera(model)
+
+if(args.remote_server != None):
+	ip,port = args.remote_server.split(":")
 else:
-	detectForVideo(file,model)
+	ip,port = ''
 
-
-
-
-
-
-
-
-
-   
-    
+if(file == 'camera'):
+	detectForVideo('camera',model,args.output,ip,port)
+else:
+	detectForVideo(file, model,args.output,ip,port)
 
 # Inference
 
